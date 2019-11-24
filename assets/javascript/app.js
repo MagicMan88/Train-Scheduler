f26a2755b1a4c5029f0b06060a464b068786d900
Stephen Green <stephengreen1028@gmail.com>
2019-11-23 22:46:52 -0700

    added correct app.js file, added firebaseConfig to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Main Processes
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCPkQWxhEy1WYRewFrqqQr5gMrqNIkwz5w",
+    authDomain: "train-scheduler-5b429.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5b429.firebaseio.com",
+    projectId: "train-scheduler-5b429",
+    storageBucket: "train-scheduler-5b429.appspot.com",
+    messagingSenderId: "1099509239125",
+    appId: "1:1099509239125:web:8d28ca819dccc03a09b3dc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
